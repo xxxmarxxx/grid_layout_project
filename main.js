3dacf0c0e43f0f7fdbc7955294a33b99777672e4
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-04-15 12:26:57 +0200

    add ball

---
+++ main.js
@@ -0,0 +1,11 @@
+// start ball animation
+var f = document.getElementById("foo");
+document.addEventListener(
+  "click",
+  function (ev) {
+    f.style.transform = "translateY(" + (ev.clientY - 25) + "px)";
+    f.style.transform += "translateX(" + (ev.clientX - 25) + "px)";
+  },
+  false
+);
+// and
